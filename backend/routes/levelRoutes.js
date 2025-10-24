97f8540fb33dc13811a6a2cfa059135008b16082
Divyatej-2024 <158854072+Divyatej-2024@users.noreply.github.com>
2025-10-24 08:10:46 +0100

    Resolved merge conflict in levelRoutes.js

--- backend/routes/levelRoutes.js
+++ backend/routes/levelRoutes.js
@@ -1,21 +1,9 @@
-<<<<<<< HEAD
 // backend/routes/levelRoutes.js
 const express = require('express');
+const { getLevels, createLevel } = require('../controllers/levelController');
 const router = express.Router();
-const { getLevels, getLevelById } = require('../controllers/levelController');
 
 router.get('/', getLevels);
-router.get('/:id', getLevelById);
+router.post('/', createLevel);
 
 module.exports = router;
-=======
-// backend/routes/levelRoutes.js
-const express = require('express');
-const router = express.Router();
-const { getLevels, getLevelById } = require('../controllers/levelController');
-
-router.get('/', getLevels);
-router.get('/:id', getLevelById);
-
-module.exports = router;
->>>>>>> 622a2f58a21cb608242fa59b8e4c35dfb00d67b0
