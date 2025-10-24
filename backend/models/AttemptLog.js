6473f29db3e16a8dfb76bd6af3962e767f695d74
Divyatej-2024 <158854072+Divyatej-2024@users.noreply.github.com>
2025-10-24 08:47:23 +0100

    Resolved merge conflict in AttemptLog.js

--- backend/models/AttemptLog.js
+++ backend/models/AttemptLog.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // backend/models/AttemptLog.js
 const mongoose = require('mongoose');
 
@@ -16,22 +15,3 @@ const AttemptLogSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('AttemptLog', AttemptLogSchema);
-=======
-// backend/models/AttemptLog.js
-const mongoose = require('mongoose');
-
-const AttemptLogSchema = new mongoose.Schema({
-  userId: String,
-  level: Number,
-  action: String,
-  timeTaken: Number,
-  hintsUsed: Number,
-  mlPrediction: String,
-  mlConfidence: Number,
-  correct: Boolean,
-  xpAwarded: Number,
-  timestamp: Date
-});
-
-module.exports = mongoose.model('AttemptLog', AttemptLogSchema);
->>>>>>> 622a2f58a21cb608242fa59b8e4c35dfb00d67b0
