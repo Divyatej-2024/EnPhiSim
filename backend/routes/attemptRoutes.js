8d1a3ad028cb78836a5f5f7e9ddc445ca4fb5fc0
Divyatej-2024 <158854072+Divyatej-2024@users.noreply.github.com>
2025-10-24 08:24:24 +0100

    Resolved merge conflict in attemptRoutes.js

--- backend/routes/attemptRoutes.js
+++ backend/routes/attemptRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // backend/routes/attemptRoutes.js
 const express = require('express');
 const router = express.Router();
@@ -7,7 +6,6 @@ const { submitAttempt } = require('../controllers/attemptController');
 router.post('/:level', submitAttempt);
 
 module.exports = router;
-=======
 // backend/routes/attemptRoutes.js
 const express = require('express');
 const router = express.Router();
@@ -16,4 +14,3 @@ const { submitAttempt } = require('../controllers/attemptController');
 router.post('/:level', submitAttempt);
 
 module.exports = router;
->>>>>>> 622a2f58a21cb608242fa59b8e4c35dfb00d67b0
