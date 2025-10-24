fddd97dea3cad7460c5c891fa9daf6b180625342
Divya tej Pendela <158854072+Divyatej-2024@users.noreply.github.com>
2025-10-24 07:11:27 +0100

    Delete backend directory

--- backend/routes/attemptRoutes.js
+++
@@ -1,8 +0,0 @@
-// backend/routes/attemptRoutes.js
-const express = require('express');
-const router = express.Router();
-const { submitAttempt } = require('../controllers/attemptController');
-
-router.post('/:level', submitAttempt);
-
-module.exports = router;
--- backend/routes/levelRoutes.js
+++
@@ -1,9 +0,0 @@
-// backend/routes/levelRoutes.js
-const express = require('express');
-const router = express.Router();
-const { getLevels, getLevelById } = require('../controllers/levelController');
-
-router.get('/', getLevels);
-router.get('/:id', getLevelById);
-
-module.exports = router;
