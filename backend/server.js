74068b18617b2c0fd690319be822c650a8375f0c
Divya tej Pendela <158854072+Divyatej-2024@users.noreply.github.com>
2025-10-24 22:35:12 +0100

    Refactor server.js to use ES6 imports

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,23 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 
+import dotenv from "dotenv"
 dotenv.config();
 connectDB();
+
+import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("✅ MongoDB connected"))
+.catch(err => console.error("❌ MongoDB connection error:", err));
 
 app.use('/api/levels', require('./routes/levelRoutes'));
 app.use('/api/attempts', require('./routes/attemptRoutes'));
