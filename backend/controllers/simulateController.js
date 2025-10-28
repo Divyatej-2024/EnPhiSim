f5e23d6a1ab82b49d8a91fa46cabf8d8514a7d73
Divyatej-2024 <158854072+Divyatej-2024@users.noreply.github.com>
2025-10-28 16:58:48 +0000

    backend

---
+++ backend/controllers/simulateController.js
@@ -0,0 +1,1 @@
+#simulateController.js
--- backend/routes/levelRoutes.js
+++
@@ -1,9 +0,0 @@
-// backend/routes/levelRoutes.js
-const express = require('express');
-const { getLevels, createLevel } = require('../controllers/LevelController');
-const router = express.Router();
-
-router.get('/', getLevels);
-router.post('/', createLevel);
-
-module.exports = router;
