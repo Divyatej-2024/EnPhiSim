05d9d620bd2cb1e4a6c97a05628e9f7156fe46ad
Divya tej Pendela <158854072+Divyatej-2024@users.noreply.github.com>
2025-10-29 20:28:42 +0000

    Add Home component with welcome message

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,1 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '3rem' }}>
+      <h1>Welcome to EnPhiSim</h1>
+      <p>Your phishing simulation and awareness platform.</p>
+    </div>
+  );
+}
+
+export default Home;
 
