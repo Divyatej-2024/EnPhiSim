0b9a642122f97145c8f49444cd822c7ac8975f42
Divya tej Pendela <158854072+Divyatej-2024@users.noreply.github.com>
2025-10-31 15:07:24 +0000

    Add image to Home component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '3rem' }}>
+      <img src="./public/Enphisim.png">
       <h1>Welcome to EnPhiSim</h1>
       <p>Your phishing simulation and awareness platform.</p>
       <a href="Simulation.jsx">Let's Begin</a>
