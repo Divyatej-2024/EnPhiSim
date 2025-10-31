159969ca81cf868829aa391d74065eb4b0fb21b2
Divya tej Pendela <158854072+Divyatej-2024@users.noreply.github.com>
2025-10-31 15:10:07 +0000

    Update image source to use imported logo

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import EnphisimLogo from '../assests/Enphisim.png';
 function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '3rem' }}>
-      <img src="./public/Enphisim.png">
+      <img src={EnphisimLogo} alt="EnPhiSim Logo" width="150">
       <h1>Welcome to EnPhiSim</h1>
       <p>Your phishing simulation and awareness platform.</p>
       <a href="Simulation.jsx">Let's Begin</a>
