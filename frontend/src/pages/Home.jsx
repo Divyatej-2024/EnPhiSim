5314316e3a0c7254e470832bfaf326a7acddf920
Divya tej Pendela <158854072+Divyatej-2024@users.noreply.github.com>
2025-10-31 15:17:59 +0000

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import EnphisimLogo from '../Enphisim.png';
+import {Link} from 'react-router-dom';
 function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '3rem' }}>
-      <img src={EnphisimLogo} alt="EnPhiSim Logo" width="150"/>
+      <img src={'${process.env.PUBLIC_URL}/Enphisim.png'} alt="EnPhiSim Logo" width="150"/>
       <h1>Welcome to EnPhiSim</h1>
       <p>Your phishing simulation and awareness platform.</p>
       <a href="Simulation.jsx">Let's Begin</a>
