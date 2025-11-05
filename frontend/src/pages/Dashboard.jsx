40adbe851779177efa6dac0587c9381ebb1c99ac
Divyatej-2024 <158854072+Divyatej-2024@users.noreply.github.com>
2025-11-05 06:10:36 +0000

    frontend

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 // src/pages/Dashboard.jsx
 import React, { useMemo } from "react";
 import { Link } from "react-router-dom";
-import { useProgress } from "../context/ProgressContext";
+import { useProgress } from "./context/ProgressContext";
 
 /*
   Adjust these links if you used different paths. These match the structure we agreed:
