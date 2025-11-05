ef72af8bc79e23f77592ceea09353b32425affdd
Divya tej Pendela <158854072+Divyatej-2024@users.noreply.github.com>
2025-11-05 18:25:05 +0000

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@
 import React, { useMemo } from "react";
 import { Link } from "react-router-dom";
 import { useProgress } from "../context/ProgressContext";
-//import { levels } from "./levels/level_data.json";
+//import { levels } from "./levels/level_data";
 
 const START_LINKS = [
   { label: "Easy", path: "/levels/easy/l1" },
