cb491ff802f8bffc7b513bf871e9044b5efad75e
Divya tej Pendela <158854072+Divyatej-2024@users.noreply.github.com>
2025-11-05 18:56:14 +0000

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 // src/App.jsx
 import React from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Disclaimer from "./pages/Disclaimer";
 import Dashboard from "./pages/Dashboard";
@@ -7,7 +8,6 @@ import LevelPage from "./pages/LevelPage";
 import Home from "./pages/Home"
 
 
-// static redirects
 export default function App() {
   return (
     <BrowserRouter>
@@ -16,23 +16,9 @@ export default function App() {
         <Route path="/disclaimer" element={<Disclaimer />} />
         <Route path="/dashboard" element={<Dashboard />} />
 
-        {/* easy */}
-        <Route path="/levels/easy/:levelId" element={<LevelPage />} />
-        {/* adv_easy */}
-        <Route path="/levels/adv_easy/:levelId" element={<LevelPage />} />
-        {/* normal */}
-        <Route path="/levels/normal/:levelId" element={<LevelPage />} />
-        {/* prehard */}
-        <Route path="/levels/prehard/:levelId" element={<LevelPage />} />
-        {/* hard */}
-        <Route path="/levels/hard/:levelId" element={<LevelPage />} />
-        {/* adv_hard */}
-        <Route path="/levels/adv_hard/:levelId" element={<LevelPage />} />
-        {/* final */}
-        <Route path="/levels/final/:levelId" element={<LevelPage />} />
-        {/* bonus */}
-        <Route path="/levels/bonus/:levelId" element={<LevelPage />} />
-
+        {/* 👇👇👇 THE FIX: Captures BOTH category and levelId parameters 👇👇👇 */}
+        <Route path="/levels/:category/:levelId" element={<LevelPage />} />
+        
         {/* catch-all */}
         <Route path="*" element={<div style={{ padding: 24, color: "#fff" }}>Page not found</div>} />
       </Routes>
