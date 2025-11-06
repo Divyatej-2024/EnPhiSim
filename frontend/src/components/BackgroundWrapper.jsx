259b5817feed65e1e906968ad3dd44addec6ed8a
Divyatej-2024 <158854072+Divyatej-2024@users.noreply.github.com>
2025-11-06 17:30:59 +0000

    frontEnd

--- frontend/src/components/BackgroundWrapper.jsx
+++ frontend/src/components/BackgroundWrapper.jsx
@@ -1,6 +1,21 @@
 import React from "react";
-import "./BackgroundWrapper.css";
+//import "./BackgroundWrapper.css";
 
 export default function BackgroundWrapper({ children }) {
-  return <div className="bg-wrapper">{children}</div>;
+  return (
+    <div
+      className="bg-wrapper"
+      style={{
+        backgroundImage: "url(" + process.env.PUBLIC_URL + '/Enphisim.png' + ")",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundAttachment: "fixed",
+        backgroundSize: "400px auto",
+        opacity: 0.06,
+        zIndex: -1,
+      }}
+    >
+      {children}
+    </div>
+  );
 }
