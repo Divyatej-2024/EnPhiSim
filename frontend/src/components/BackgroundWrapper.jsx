5954de0d04a02e034469ea40cf50d3cc7ec5e154
Divyatej-2024 <158854072+Divyatej-2024@users.noreply.github.com>
2025-11-06 17:38:32 +0000

    frontEnd

--- frontend/src/components/BackgroundWrapper.jsx
+++ frontend/src/components/BackgroundWrapper.jsx
@@ -1,20 +1,31 @@
 import React from "react";
-//import "./BackgroundWrapper.css";
 
 export default function BackgroundWrapper({ children }) {
+  const wrapperStyle = {
+    position: "relative",
+    minHeight: "100vh",
+    overflow: "hidden",
+  };
+
+  const fadedLogoStyle = {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    backgroundImage: `url(${process.env.PUBLIC_URL}/Enphisim.png)`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    backgroundAttachment: "fixed",
+    backgroundSize: "400px auto",
+    opacity: 0.06,
+    zIndex: -1,
+    pointerEvents: "none",
+  };
+
   return (
-    <div
-      className="bg-wrapper"
-      style={{
-        backgroundImage: "url(" + process.env.PUBLIC_URL + '/Enphisim.png' + ")",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-        backgroundAttachment: "fixed",
-        backgroundSize: "400px auto",
-        opacity: 0.6,
-        zIndex: -1,
-      }}
-    >
+    <div style={wrapperStyle}>
+      <div style={fadedLogoStyle}></div>
       {children}
     </div>
   );
