e81a08af0b128843411b125d0f2e66c7f22d4b66
Divyatej-2024 <158854072+Divyatej-2024@users.noreply.github.com>
2025-11-14 15:29:58 +0000

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,39 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
 import cors from 'cors';
-import axios from 'axios';
 
-dotenv.config();
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect("mongodb+srv://pdivyatej2003_db_user:TEJ_2025@enphisimdb.objuvdc.mongodb.net/?appName=EnPhiSimdb")
   .then(() => console.log('✅ MongoDB Connected'))
   .catch((err) => console.error('❌ MongoDB Error:', err));
 
+const levelSchema = new mongoose.Schema({}, {strict:false});
+const Level = mongoose.model("Level",levelSchema, "levelsDataset");
+
+app.get("/api/levels", async (req,res) =>{
+try{
+  const levels = await Level.find();
+  res.json(levels);
+}
+catch (err) {
+  res.status(500).json({message: err.message});
+}
+}
+  );
+
 // Example route that talks to ML Server
-app.post('/api/predict', async (req, res) => {
+app.post('/api/levels/:id', async (req, res) => {
   try {
-    const response = await axios.post(`${process.env.ML_SERVER_URL}/predict`, req.body);
-    res.json(response.data);
+    const level = await Level.findById(req.params.id);
+    res.json(level);
   } catch (err) {
-    console.error('ML Server error:', err.message);
-    res.status(500).json({ error: 'ML Server failed to respond' });
+    res.status(404).json({ error: 'Level not found' });
   }
 });
 
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log(`🚀 Backend running on port ${PORT}`));
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
