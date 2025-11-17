5b48681016ec8b87c832bf7fd5e1cb87b73f225e
Divya tej Pendela <158854072+Divyatej-2024@users.noreply.github.com>
2025-11-17 17:51:40 +0000

    Update l1.jsx

--- frontend/src/pages/levels/easy/l1.jsx
+++ frontend/src/pages/levels/easy/l1.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../../level.css";
+import "../../level-mail.css";
 import ".levels/l1.css";
 export default function Level1({ level, onOptionClick }) {
   return (
