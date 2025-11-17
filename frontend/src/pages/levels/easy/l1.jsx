64e95caccdd01bad12eff939fc17cb6ae32152dc
Divyatej-2024 <158854072+Divyatej-2024@users.noreply.github.com>
2025-11-17 21:06:40 +0000

    frontEND

--- frontend/src/pages/levels/easy/l1.jsx
+++ frontend/src/pages/levels/easy/l1.jsx
@@ -2,18 +2,31 @@ import React from "react";
 import "../../../level.css";
 import "../../../level-mail.css";
 import "./l1.css";
+
 export default function Level1({ level, onOptionClick }) {
   return (
-    <div className="level-container">
-      <h1>{level.page_title}</h1>
+    <div className="level-mail-wrapper">
+      <div className="mail-header">
+        <div className="mail-from">
+          <strong>{level.from || "IT Support"}</strong>
+          <span>{level.email || "it-support@university.ac.uk"}</span>
+        </div>
+        <div className="mail-meta">
+          <span>{level.date || "Today"}</span>
+        </div>
+      </div>
+
+      <div className="mail-subject">
+        {level.page_title}
+      </div>
 
       <div
-        className="email-content"
+        className="mail-body"
         dangerouslySetInnerHTML={{ __html: level.content }}
       />
 
       <div className="options">
-        {level.options.map((opt) => (
+        {level.options?.map((opt) => (
           <button key={opt.key} onClick={() => onOptionClick(opt)}>
             {opt.label}
           </button>
