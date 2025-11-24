46bbbb0ded503c9f272774737a063721af00e30f
Divya tej Pendela <158854072+Divyatej-2024@users.noreply.github.com>
2025-11-24 15:17:55 +0000

    Update disclaimer styling by removing background image
    Removed background image from disclaimer paragraph.

--- frontend/src/pages/Disclaimer.jsx
+++ frontend/src/pages/Disclaimer.jsx
@@ -1,7 +1,7 @@
 // src/pages/Disclaimer.jsx
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-//import "../level.css";
+import "../level.css";
 
 export default function Disclaimer() {
   const [agreed, setAgreed] = useState(false);
@@ -10,7 +10,7 @@ export default function Disclaimer() {
     <div style={{ minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center", color: "#e6eef8", padding: 24 }}>
       <div style={{ background: "#071029", padding: 28, borderRadius: 12, maxWidth: 500, opacity:0.5 }}>
         <h1> EnPhiSim — Disclaimer</h1>
-        <p style={{ backgroundImage:`url(${process.env.PUBLIC_URL}/img/avtar.png)`,color: "#fff",backgroundRepeat:"no-repeat" }}>
+        <p style={{ color: "#fff",backgroundRepeat:"no-repeat" }}>
 Welcome to EnPhiSim — a training-only simulation environment.
 
 EnPhiSim is a completely virtual world of simulation: every scenario, challenge, and simulated attack exists solely to train, demonstrate, and analyze cybersecurity threats in a safe, controlled setting.
