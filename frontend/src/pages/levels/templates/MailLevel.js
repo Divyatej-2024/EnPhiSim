cbccc72b9be717a13f5156c137c80790c6e34bf1
Divya tej Pendela <158854072+Divyatej-2024@users.noreply.github.com>
2025-12-03 13:18:09 +0000

    Update MailLevel.js

--- frontend/src/pages/levels/templates/MailLevel.js
+++ frontend/src/pages/levels/templates/MailLevel.js
@@ -92,7 +92,7 @@ Hint: ${level.Hint || "No hint provided"}`;
           backgroundColor: "#fff",
           color: "#333",
           fontFamily: "Arial",
-          margin-top:"500px",
+          margin-top:"500px"
           margin-right:"500px",
           margin-left:"500px",
           position: "relative",
