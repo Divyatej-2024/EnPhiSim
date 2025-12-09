381fead6518cd6ec93244be00434c61558199037
Divyatej-2024 <158854072+Divyatej-2024@users.noreply.github.com>
2025-12-09 17:27:17 +0000

    live server backend

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import mlRoutes from "./routes/mlanalysis.js";
-app.use("/api", mlRoutes);
+
 
 
 dotenv.config();
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use("/api", mlRoutes);
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI,{
  UserNewUrlParser: true,
