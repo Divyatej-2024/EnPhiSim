d828e8c6331b01f32b034a709f87db7563ef94c8
Divyatej-2024 <158854072+Divyatej-2024@users.noreply.github.com>
2025-12-11 05:26:52 +0000

    updates

--- backend/models/Level.js
+++ backend/models/Level.js
@@ -1,23 +1,25 @@
 import mongoose from "mongoose";
 
-const LevelSchema = new mongoose.Schema({
-  level: { type: Number, required: true, unique: true },
-  title: String,
-  difficulty: String,
-  category: String,
-  description: String,
-  sampleEmail: {
-    subject: String,
-    body: String,
-    from: String,
+const LevelSchema = new mongoose.Schema(
+  {
+    level: { type: Number, required: true, unique: true },
+    title: String,
+    difficulty: String,
+    category: String,
+    description: String,
+    sampleEmail: {
+      subject: String,
+      body: String,
+      from: String,
+    },
+    correctAction: String,
+    baseXP: Number,
+    hint: [String],
+    ml_confidence_threshold: Number,
+    tags: [String],
   },
-  correctAction: String,
-  baseXP: Number,
-  hint: [String],
-  ml_confidence_threshold: Number,
-  tags: [String]
-}, { collection: "levelDataset" }); // important
+  { collection: "levelDataset" }
+);
 
 const Level = mongoose.model("Level", LevelSchema);
-
 export default Level;
