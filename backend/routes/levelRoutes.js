aa5e8bb25ff3a1b9c8f981b7257ee3e4545810bf
Divyatej-2024 <158854072+Divyatej-2024@users.noreply.github.com>
2026-02-19 22:23:52 +0000

    backend

--- backend/routes/levelRoutes.js
+++ backend/routes/levelRoutes.js
@@ -3,10 +3,45 @@ import Level from "../models/Level.js";
 
 const router = express.Router();
 
+
+// Get all levels (for dashboard only)
 router.get("/levels", async (req, res) => {
   try {
-    const levels = await Level.find({});
+    const levels = await Level.find({}, { scenarios: 0 }); 
+    // Exclude scenarios for performance
     res.json(levels);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
+// Get ONE level with ONE random scenario
+router.get("/levels/:level_id", async (req, res) => {
+  try {
+    const { levelId } = req.params;
+
+    const level = await Level.findOne({ levelId });
+
+    if (!level) {
+      return res.status(404).json({ message: "Level not found" });
+    }
+
+    if (!level.scenarios || level.scenarios.length === 0) {
+      return res.status(404).json({ message: "No scenarios found" });
+    }
+
+    // Random scenario
+    const randomIndex = Math.floor(Math.random() * level.scenarios.length);
+    const randomScenario = level.scenarios[randomIndex];
+
+    res.json({
+      level_id: level.level_id,
+      title: level.title,
+      type: level.type,
+      scenario: randomScenario
+    });
+
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
