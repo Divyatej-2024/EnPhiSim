016e020cf2739903afb2ec0da50a5a5137bedee0
Divya tej Pendela <158854072+Divyatej-2024@users.noreply.github.com>
2026-02-21 01:29:22 +0000

    Update API URL for fetching mail scenario

--- frontend/src/pages/levels/templates/MailLevel.js
+++ frontend/src/pages/levels/templates/MailLevel.js
@@ -19,7 +19,7 @@ export default function MailLevel({levelId}) {
     }
 
     // Fetch ONE scenario from the server
-    fetch(`{REACT_APP_API_URL}/api/mailScenario?levelId=${levelId}`)
+    fetch(`{MONGO_URL}/api/mailScenario?levelId=${levelId}`)
       .then(res => res.json())
       .then(scenario => {
         // Save to progress context
